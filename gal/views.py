3c4be3e1c1d7c51eaf197c29bc12d591a3335e78
bri <briegrant416@gmail.com>
2018-03-06 20:11:38 +0300

    define images to views.py

--- gal/views.py
+++ gal/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render,HttpResponse
 from .models import Category,Location,Images
 
 # Create your views here.
+def index (request):
+    images = Image.get_images()
+    return render(request,'index.html',{"images":images})
