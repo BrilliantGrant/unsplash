6e75e0aabdb4b45c88f87f1177c1d485bbe0a833
bri <briegrant416@gmail.com>
2018-03-06 19:46:46 +0300

    Add class Image with further details of saving and deleting to models.py

--- gal/models.py
+++ gal/models.py
@@ -8,8 +8,29 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+    def delete_location(self):
+        self.delete()
+
     def save_location(self):
         self.save()
 
-    def delete_location(self):
+    
+
+class Images(models.Model):
+	descripton = models.TextField()
+    location = models.ForeignKey(Location, null=True)
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to = 'images/', null = True)
+    time = models.DateTimeField(auto_now_add=True, null=True)
+   
+    # category = models.ManyToManyField(categories)
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
         self.delete()
+
+    @classmethod
+    def search_by_title(cls, search_term):
+        gallery = cls.objects.filter(descripton__icontains=search_term)
+        return gallery
