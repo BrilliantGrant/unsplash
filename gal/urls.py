04671180d7d6d7b4bbbcebfb04782d8bd75e1602
bri <briegrant416@gmail.com>
2018-03-06 20:22:07 +0300

    Add the url patterns to urls.py

--- gal/urls.py
+++ gal/urls.py
@@ -2,3 +2,11 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns=[
+    url(r'^image/(\d+)',views.image,name='image'),
+    url('^$',views.index, name='index'),
+    url(r'^search/',views.search_results,name='search_results'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
